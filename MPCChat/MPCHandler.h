42c4e56e66ff4df1c16ce2e2c67611e9bee4c1da
Chengzhao Li <leslie_lcz@hotmail.com>
2016-02-24 16:39:16 -0600

    - MPC Chat

---
+++ MPCChat/MPCHandler.h
@@ -0,0 +1,13 @@
+//
+//  MPCHandler.h
+//  MPCChat
+//
+//  Created by Chengzhao Li on 2016-02-24.
+//  Copyright © 2016 Chengzhao Li. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MPCHandler : NSObject
+
+@end
---
+++ MPCChat/MPCHandler.m
@@ -0,0 +1,13 @@
+//
+//  MPCHandler.m
+//  MPCChat
+//
+//  Created by Chengzhao Li on 2016-02-24.
+//  Copyright © 2016 Chengzhao Li. All rights reserved.
+//
+
+#import "MPCHandler.h"
+
+@implementation MPCHandler
+
+@end
